fef617ff73ca26e3eff4098b58a6fb3a66bec607
Chris Mowforth <chris@mowforth.com>
2014-06-08 13:20:23 +0100

    pure levenshtein impl

---
+++ src/main/clojure/com/mowforth/dynamic/levenshtein.clj
@@ -0,0 +1,14 @@
+(ns com.mowforth.dynamic.leveshtein
+  (:use [clojure.core.match :only (match)]))
+
+(defn pure [a b]
+  "A simple recursive definition of the Levenshtein function"
+  (match [(count a) (count b)]
+         [0 _] (count b)
+         [_ 0] (count a)
+         [_ _] (let [cost (if
+                            (= (last a) (last b)) 0
+                            1)]
+                 (apply min [(+ (pure (butlast a) b) 1)
+                             (+ (pure a (butlast b)) 1)
+                             (+ (pure (butlast a) (butlast b)) cost)]))))

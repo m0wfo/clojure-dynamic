c3fa1f3b1480faf637be14d8ce72773ed6a1ab58
Chris Mowforth <chris@mowforth.com>
2014-06-08 13:42:55 +0100

    add nrepl symbol completion and top-down levenshtein impl

--- src/main/clojure/com/mowforth/dynamic/levenshtein.clj
+++ src/main/clojure/com/mowforth/dynamic/levenshtein.clj
@@ -6,9 +6,14 @@
   (match [(count a) (count b)]
          [0 _] (count b)
          [_ 0] (count a)
-         [_ _] (let [cost (if
-                            (= (last a) (last b)) 0
-                            1)]
+         [_ _] (let [cost (if (= (last a) (last b)) 0 1)]
                  (apply min [(+ (pure (butlast a) b) 1)
                              (+ (pure a (butlast b)) 1)
                              (+ (pure (butlast a) (butlast b)) cost)]))))
+
+(defn top-down [a b]
+  (with-redefs [pure (memoize pure)]
+    (pure a b)))
+
+(defn bottom-up [a b]
+  )
